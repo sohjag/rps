f3e18ef692de43f0922dad543a1d7bd047a14717
Sohan <sohan.j12@gmail.com>
2023-10-25 15:31:57 +0530

    added salt generation function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,26 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useMoralis, useWeb3Contract } from "react-moralis";
 import Navbar from "@/components/Navbar";
+import generateSalt from "@/utils/generateSalt";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { enableWeb3, isWeb3Enabled } = useMoralis();
   console.log("...isweb3 enabled", isWeb3Enabled);
+
+  const handleSaltGeneration = () => {
+    const salt = generateSalt();
+    console.log("generated salt is...", salt);
+  };
+
   return (
     <div>
       <Navbar />
       <div>Hello world</div>
+      <div>
+        <button onClick={handleSaltGeneration}>Generate Salt</button>
+      </div>
     </div>
   );
 }
---
+++ src/utils/generateSalt.ts
@@ -0,0 +1,17 @@
+import { BN } from "bn.js";
+
+export default function generateSalt() {
+  // Generate a random 256-bit (32-byte) salt
+  const randomBytes = new Uint8Array(32);
+  window.crypto.getRandomValues(randomBytes);
+
+  // Convert the byte array to a Big-Endian uint256
+  let salt = new Uint8Array(32);
+  for (let i = 0; i < 32; i++) {
+    salt[i] = randomBytes[31 - i];
+  }
+
+  const saltUint256 = new BN(salt).toString("hex");
+
+  return saltUint256;
+}
