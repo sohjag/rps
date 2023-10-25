cda8adcafce577aba998a1a63807fea1d2ed697a
Sohan <sohan.j12@gmail.com>
2023-10-25 15:00:13 +0530

    wallet connect functionality added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useMoralis, useWeb3Contract } from "react-moralis";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello world</div>;
+  const { enableWeb3, isWeb3Enabled } = useMoralis();
+  return (
+    <div>
+      <div>
+        {isWeb3Enabled ? (
+          <div>Connected!</div>
+        ) : (
+          <div>
+            <button
+              onClick={() => {
+                enableWeb3();
+              }}
+            >
+              Connect wallet
+            </button>
+          </div>
+        )}
+      </div>
+      <div>Hello world</div>
+    </div>
+  );
 }
