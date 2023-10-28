bfa58678e35934d0f144468100c2e2f54bd0d3b7
Sohan <sohan.j12@gmail.com>
2023-10-28 15:28:50 +0530

    modified verify endpoint

--- src/pages/api/auth-test/verify.ts
+++ src/pages/api/auth-test/verify.ts
@@ -11,72 +11,73 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const { address, signature, nonce } = req.body;
-    let authenticated = false;
-    await ensureDbConnected();
-
-    //get nonce from db
-    const existingNonce = await UserNonce.findOne({
-      user_address: address,
-    });
-    const currentTime = new Date();
-
-    const tenSecondsLater = new Date(existingNonce.createdAt);
-    tenSecondsLater.setSeconds(existingNonce.createdAt.getSeconds() + 10);
-
-    if (existingNonce && tenSecondsLater > currentTime) {
-      const decodedAddress = ethers.utils.verifyMessage(
-        existingNonce.nonce,
-        signature
-      );
-      console.log("decoded address is...", decodedAddress);
-
-      if (address === decodedAddress.toLowerCase()) {
-        console.log("inside IF block...");
-        console.log("address is ....", address);
-        console.log("decoded address is...", decodedAddress.toLowerCase());
-        const jwtToken = await new jose.SignJWT({ userAddress: address })
-          .setProtectedHeader({ alg: "HS256" })
-          .setIssuedAt()
-          .setExpirationTime("1h")
-          .sign(new TextEncoder().encode(process.env.SECRET_KEY));
-
-        console.log("jwtToken is...", jwtToken);
-
-        // const cookies = cookie.serialize("rps-token", jwtToken, {
-        //   httpOnly: true,
-        //   maxAge: 3600, // 1 hour in seconds
-        //   sameSite: "strict",
-        //   path: "/",
-        // });
-
-        // console.log("cookies created...", cookies);
-
-        // res.setHeader("Set-Cookie", cookies);
-
-        // console.log("response header set with cookies");
-
-        // new db schema not being sconstructed in vercel -- check back later
-        // //check if user exists
-        // const existingUser = await User.findOne({
-        //   user_address: address,
-        // });
-        // console.log("checked if user exists");
-
-        // //if not,create new
-        // if (!existingUser) {
-        //   const userObj = new User({ user_address: address });
-        //   await userObj.save();
-        // }
-
-        authenticated = true;
-        res.status(200).json({ authenticated, jwtToken });
-      }
-      res.status(400).json({ authenticated });
-    }
-    res.status(401).json({ message: "Nonce expired or not found" });
-
     try {
+      const { address, signature, nonce } = req.body;
+      let authenticated = false;
+      await ensureDbConnected();
+
+      //get nonce from db
+      const existingNonce = await UserNonce.findOne({
+        user_address: address,
+      });
+      const currentTime = new Date();
+
+      const tenSecondsLater = new Date(existingNonce.createdAt);
+      tenSecondsLater.setSeconds(existingNonce.createdAt.getSeconds() + 10);
+
+      if (existingNonce && tenSecondsLater > currentTime) {
+        const decodedAddress = ethers.utils.verifyMessage(
+          existingNonce.nonce,
+          signature
+        );
+        console.log("decoded address is...", decodedAddress);
+
+        if (address === decodedAddress.toLowerCase()) {
+          console.log("inside IF block...");
+          console.log("address is ....", address);
+          console.log("decoded address is...", decodedAddress.toLowerCase());
+          const jwtToken = await new jose.SignJWT({ userAddress: address })
+            .setProtectedHeader({ alg: "HS256" })
+            .setIssuedAt()
+            .setExpirationTime("1h")
+            .sign(new TextEncoder().encode(process.env.SECRET_KEY));
+
+          console.log("jwtToken is...", jwtToken);
+
+          const cookies = cookie.serialize("rps-token", jwtToken, {
+            httpOnly: true,
+            maxAge: 3600, // 1 hour in seconds
+            sameSite: "strict",
+            path: "/",
+          });
+
+          console.log("cookies created...", cookies);
+
+          res.setHeader("Set-Cookie", cookies);
+
+          console.log("response header set with cookies");
+
+          //check if user exists
+          const existingUser = await User.findOne({
+            user_address: address,
+          });
+          console.log("checked if user exists");
+
+          //if not,create new
+          if (!existingUser) {
+            console.log("user not found..creating new user");
+            const userObj = new User({ user_address: address });
+            await userObj.save();
+          }
+
+          authenticated = true;
+
+          console.log("authenticated set to true...");
+          return res.status(200).json({ authenticated, jwtToken });
+        }
+        return res.status(400).json({ authenticated });
+      }
+      return res.status(401).json({ message: "Nonce expired or not found" });
     } catch (e) {
       console.log(e);
       res.status(500).json({ message: "internal server error" });
